7e0cb7b0d31a59b23ce2be995e61f4a4e5076b89
trumandaniels <trumanadaniels@gmail.com>
2024-12-08 23:52:01 -0800

    Added navbar.js, basic navigation menu (no style) and updated html files with navbar script

---
+++ navbar.js
@@ -0,0 +1,32 @@
+const header = document.createElement('header');
+
+//All Menu Links
+const items = [
+    { href: 'about.html', text: 'About' },
+    { href: 'contact.html', text: 'Contact' },
+    { href: 'previous-work.html', text: 'Previous Work' },
+    { href: 'projects.html', text: 'Projects' },
+  ];
+
+const navList = document.createElement('ul');
+navList.className = 'nav-list';
+
+items.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.className = 'nav-list-item';
+
+    const link = document.createElement('a');
+    link.href = item.href;
+    link.className = 'nav-link hover-transition';
+    link.textContent = item.text;
+
+    listItem.appendChild(link);
+    navList.appendChild(listItem);
+});
+
+const largeScreenNavbar = document.createElement('nav');
+largeScreenNavbar.className = 'navbar large-screen-navbar';
+largeScreenNavbar.appendChild(navList);
+
+header.appendChild(largeScreenNavbar);
+document.body.appendChild(header);

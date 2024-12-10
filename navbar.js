50ee96259702500a4242da395bc4c3d7ccaa4f7f
trumandaniels <trumanadaniels@gmail.com>
2024-12-10 08:07:54 -0800

    Fixed many small issues, formatted code, validated css and html

--- navbar.js
+++ navbar.js
@@ -1,61 +1,61 @@
 const createNavList = (items) => {
-    const navList = document.createElement('ul');
-    navList.className = 'nav-list';
+  const navList = document.createElement("ul");
+  navList.className = "nav-list";
 
-    items.forEach(item => {
-        const listItem = document.createElement('li');
-        listItem.className = 'nav-list-item';
+  items.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.className = "nav-list-item";
 
-        const link = document.createElement('a');
-        link.href = item.href;
-        link.className = 'nav-link hover-transition';
-        link.textContent = item.text;
+    const link = document.createElement("a");
+    link.href = item.href;
+    link.className = "nav-link hover-transition";
+    link.textContent = item.text;
 
-        listItem.appendChild(link);
-        navList.appendChild(listItem);
-    });
+    listItem.appendChild(link);
+    navList.appendChild(listItem);
+  });
 
-    return navList;
+  return navList;
 };
 
-const header = document.createElement('header');
+const header = document.createElement("header");
 
 // All Menu Links
 const items = [
-    { href: 'about.html', text: 'About Me' },
-    { href: 'contact.html', text: 'Contact' },
-    { href: 'previous-work.html', text: 'Previous Work' },
-    { href: 'projects.html', text: 'Projects' },
+  { href: "projects.html", text: "Projects" },
+  { href: "previous-work.html", text: "Experience" },
+  { href: "about.html", text: "About Me" },
+  { href: "contact.html", text: "Contact" },
 ];
 
-const largeScreenNavbar = document.createElement('nav');
-largeScreenNavbar.className = 'navbar large-screen-navbar';
+const largeScreenNavbar = document.createElement("nav");
+largeScreenNavbar.className = "navbar large-screen-navbar";
 largeScreenNavbar.appendChild(createNavList(items));
 
-const smallScreenNavbar = document.createElement('nav');
-smallScreenNavbar.className = 'navbar small-screen-navbar';
+const smallScreenNavbar = document.createElement("nav");
+smallScreenNavbar.className = "navbar small-screen-navbar";
 
 // Create the checkbox to toggle the navbar
-const toggleTrigger = document.createElement('input');
-toggleTrigger.type = 'checkbox';
-toggleTrigger.className = 'navbar-toggle-trigger';
-toggleTrigger.id = 'navbar-toggle-trigger';
+const toggleTrigger = document.createElement("input");
+toggleTrigger.type = "checkbox";
+toggleTrigger.className = "navbar-toggle-trigger";
+toggleTrigger.id = "navbar-toggle-trigger";
 smallScreenNavbar.appendChild(toggleTrigger);
 
 // label the hamburger menu
-const toggleExpandButton = document.createElement('label');
-toggleExpandButton.setAttribute('for', 'navbar-toggle-trigger');
-toggleExpandButton.className = 'navbar-circular-icon-button';
-toggleExpandButton.setAttribute('aria-label', 'open navbar list'); // improve accessibility 
-const icon = document.createElement('i');
-icon.className = 'fa-solid fa-bars'; // Add icon for fontawesome
+const toggleExpandButton = document.createElement("label");
+toggleExpandButton.setAttribute("for", "navbar-toggle-trigger");
+toggleExpandButton.className = "navbar-circular-icon-button";
+toggleExpandButton.setAttribute("aria-label", "open navbar list"); // improve accessibility
+const icon = document.createElement("i");
+icon.className = "fa-solid fa-bars"; // Add icon for fontawesome
 toggleExpandButton.appendChild(icon);
 smallScreenNavbar.appendChild(toggleExpandButton);
 
 // Create the collapsible menu container
-const smallScreenNavContainer = document.createElement('div');
-smallScreenNavContainer.id = 'small-screen-navbar-element-container';
-smallScreenNavContainer.className = 'small-screen-navbar-element-container';
+const smallScreenNavContainer = document.createElement("div");
+smallScreenNavContainer.id = "small-screen-navbar-element-container";
+smallScreenNavContainer.className = "small-screen-navbar-element-container";
 smallScreenNavbar.appendChild(smallScreenNavContainer);
 
 smallScreenNavContainer.appendChild(createNavList(items));
